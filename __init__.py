57e1a22454998a25ae6410032677ea78a697ba58
lafont-loic <loic.lafont.isr@gmail.com>
2026-01-05 17:14:44 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,7 +35,7 @@ def mongraphique():
 def monhistogramme():
     return render_template("histogramme.html")
 
-@app.route("commits" '/extract-minutes/<date_string>')
+@app.route('/extract-minutes/<date_string>')
 def extract_minutes(date_string):
         date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
         minutes = date_object.minute
