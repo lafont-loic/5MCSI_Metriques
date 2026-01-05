9fd131341f862dd5b331998aa5c3998df1363b83
lafont-loic <loic.lafont.isr@gmail.com>
2026-01-05 17:18:42 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,7 +35,7 @@ def mongraphique():
 def monhistogramme():
     return render_template("histogramme.html")
 
-@app.route('/extract-minutes/<date_string>')
+@app.route('/commits/')
 def extract_minutes(date_string):
     response = urlopen('https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits')
         date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
